643d8a9e44bc3279fc724505bb8ec76bfb24f8e4
lantoolblog <286455583+lantoolblog@users.noreply.github.com>
2026-06-09 01:07:37 +0900

    Cursor, Antigravity 호환을 위해 요구 버전 사향 낮춤
    2026/06/09 기준
    - Cursor:           1.105.1
    - Antigravity IDE:  1.107.0
    - VSCode         :  1.123.0
    
    가장 낮은 버전인 Cursor 기준으로 설정했다.

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,9 @@
-import { defineConfig } from '@vscode/test-cli';
+import { defineConfig } from "@vscode/test-cli";
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js',
+  files: "out/test/**/*.test.js",
+  // 2026/06/09 기준
+  // - Cursor:           1.105.1
+  // - Antigravity IDE:  1.107.0
+  version: "1.105.1",
 });
